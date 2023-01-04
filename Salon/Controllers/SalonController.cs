51174dc14911da2492caa5c92534ae6701b262a3
Yurii Kovalchuk <zt1552@gmail.com>
2023-01-04 10:45:37 +0200

    Add project files.

---
+++ Salon/Controllers/SalonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Salon.Controllers
+{
+    public class SalonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
